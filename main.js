42fe950cdd7d2da27207844d9184c132c563cbeb
lesavantdon <144865003+lesavantdon@users.noreply.github.com>
2023-10-03 23:10:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const findSum = function(array) {
- for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  return array[i] + array[i+1] + array[i+2];  
- }
-};
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result = result + array[i];
+    }
+    return result;
+   };
+   
 
 
 let count = {}; 
@@ -40,7 +42,7 @@ let rightToLeft= str.toLowerCase().split("").reverse().join("");
 
 const largestPair = function(array) {
   let products = [];
-for (var i=0; i < array.length; i++) {
+for (var i=0; i < array.length-1; i++) {
 var currentNum= array[i];
 var nextNum=array[i+1];
 var product = currentNum * nextNum;
@@ -54,19 +56,23 @@ return largestProduct;
 
 
 const removeParenth = function(str) {
-let splitString = str.split('');
-splitString.splice(3,5);
-return splitString.join('');
-};
+    let result = '';
+    let parenthesisStarted = false;
+    for (let i = 0; i < str.length;i++) {
+        if(str[i] === "(") {
+            parenthesisStarted = true;
+        } else if (str[i] === ")") {
+            parenthesisStarted = false;
+        } else if (parenthesisStarted === false) {
+            result += str[i];
+        }
+    }
+    return result;
+  };
 
-Problem 6
+
 const scoreScrabble = function(str) {
-let inputWord = str.toUpperCase().split('');
-  for (let i = 0; i < inputWord.length; i++) {
-    var ogInputWord = inputWord[i];
-  }
-    
-let points = {
+    let points = {
         1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
         2: ['D', 'G'],
         3: ['B', 'C', 'M', 'P'],
@@ -75,13 +81,21 @@ let points = {
         8: ['J', 'X'],
         10: ['Q', 'Z'],
       };
-          var scrabblePoints = points[Object.keys(points)[0]]
-      for (let i = 0; i < scrabblePoints.length; i++) {
-      for (prop in points) {
-       if 
-      }
-    }
-};
 
+    let inputWord = str.toUpperCase().split('');
+    let score = 0;
+      for (let i = 0; i < inputWord.length; i++) {
+        for (let key of Object.keys(points)) {
+            const values = points[key];
+            for (let j = 0; j < values.length;j++) {
+                if(values[j] === inputWord[i]) {
+                    score += Number(key);
+                    break;
+                }
+            }
+        }
+      }
+      return score;
+    };
 
 
